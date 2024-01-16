42082876a31e2c0a94488c6459bfe42f3d7c90a0
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-01-16 09:23:25 -0300

    initial project setup with eslint

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './global.css'
+
+import { RouterProvider } from 'react-router-dom'
+
+import { Button } from './components/ui/button'
+import { router } from './routes'
+export function App() {
+  return <RouterProvider router={router} />
+}
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Button>SignIn</Button>
+    </div>
+  )
+}
